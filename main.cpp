d3a11895a702af86c62c9d50352255472f2a1803
Richard Allen <rsaxvc@rsaxvc.net>
2023-12-11 23:47:55 -0600

    more iterators

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ will process the (features + timestamps) and produce the spin.
 #define DIMPLE_DIAM_RATIO_BALL_CIRC (DIMPLE_DIAM_MM/BALL_CIRC_MM)
 #define DIMPLE_DIAM_RATIO_BALL_DIAM (DIMPLE_DIAM_MM/BALL_DIAM_MM)
 #define N_DIMPLES 400 //Estimate for whole ball
-#define N_FRAMES 5 //NOTE: if we process all relationships, it's O(N*(N-1)
+#define N_FRAMES 8 //NOTE: if we process all relationships, it's O(N*(N-1)
 #define VISIBLE_RADIUS_OD .8 //Drop stuff beyond this
 #define VISIBLE_RADIUS_ID .2 //Drop stuff close to center than this(glare)
 #define DROP_RATIO .2 //drop some points randomly
@@ -32,7 +32,8 @@ will process the (features + timestamps) and produce the spin.
 
 //Configuration for solver - first takes priority
 #define USE_HOT_GARBAGE 0 //do not use hot-garbage.
-#define USE_NMS 1 //See: https://en.wikipedia.org/wiki/Nelder%E2%80%93Mead_method#One_possible_variation_of_the_NM_algorithm
+#define USE_NMS 0 //See: https://en.wikipedia.org/wiki/Nelder%E2%80%93Mead_method#One_possible_variation_of_the_NM_algorithm
+#define USE_RWALK 1
 #define USE_PROFILER 0 //infinite loop
 
 //macro functions - presentation is in RPM, but processing is rad*hz
@@ -45,14 +46,36 @@ will process the (features + timestamps) and produce the spin.
 
 struct vec3
 	{
-	float x;
-	float y;
-	float z;
+	float x;//yaw, d_yaw
+	float y;//pitch, d_pitch
+	float z;//roll, d_roll
+	vec3 operator+(const vec3 & rhs)
+		{
+		vec3 r;
+		r.x = x + rhs.x;
+		r.y = y + rhs.y;
+		r.z = z + rhs.z;
+		return r;
+		}
 	vec3 & operator+=(const vec3 & rhs)
 		{
 		this->x += rhs.x;
 		this->y += rhs.y;
 		this->z += rhs.z;
+		return *this;
+		}
+	vec3 & operator-(const vec3 & rhs)
+		{
+		this->x += rhs.x;
+		this->y += rhs.y;
+		this->z += rhs.z;
+		return *this;
+		}
+	vec3 operator*(float rhs)
+		{
+		this->x *= rhs;
+		this->y *= rhs;
+		this->z *= rhs;
 		return *this;
 		}
 	vec3 & operator/(unsigned rhs)
@@ -64,7 +87,32 @@ struct vec3
 		}
 	};
 
+vec3 operator*(float lhs, const vec3 & rhs)
+	{
+	vec3 r;
+	r.x = lhs * rhs.x;
+	r.y = lhs * rhs.y;
+	r.z = lhs * rhs.z;
+	return r;
+	}
+
 typedef std::vector<vec3> vecs;
+struct score_t
+	{
+	uint64_t score;
+	vec3 v;
+	bool operator<(const score_t &b)
+		{
+		if(score != b.score) return score < b.score;
+		return v.x < b.v.x;
+		}
+	bool operator>(const score_t &b)
+		{
+		if(score != b.score)return score > b.score;
+		return v.x > b.v.x;
+		}
+	};
+
 struct framestamp
 	{
 	uint64_t ns;
@@ -186,8 +234,8 @@ local dataset genData(unsigned n, float d_yaw, float d_pitch, float d_roll)
 		//Rotate the vectors forward in time according to current timestamp
 		framestamp frame = {ts};
 		frame.points = rotate(frame0.points, yaw, pitch, roll);
-		std::cerr<<frame.points[0].x<<frame.points[0].y<<frame.points[0].z<<std::endl;
-//		frame = filterPct(frame, DROP_RATIO);
+//		std::cerr<<frame.points[0].x<<frame.points[0].y<<frame.points[0].z<<std::endl;
+		frame = filterPct(frame, DROP_RATIO);
 //		frame = filterVisible(frame, VISIBLE_RADIUS_ID, VISIBLE_RADIUS_OD);
 
 		ret.frames.push_back(frame);
@@ -313,7 +361,12 @@ local uint64_t evalScore(const dataset & data, float d_yaw, float d_pitch, float
 	return accum;
 }
 
-local void findBest
+local uint64_t evalScore(const dataset & data, const vec3 & v)
+{
+return evalScore(data, v.x, v.y, v.z);
+}
+
+local void findBestCoarse
 	(
 	const dataset & data,
 	float yaw,
@@ -328,7 +381,8 @@ local void findBest
 	uint64_t & best_score,
 	float & best_d_yaw,
 	float & best_d_pitch,
-	float & best_d_roll
+	float & best_d_roll,
+	std::vector<score_t> & results
 	)
 {
 	for( float d_yaw = yaw - yaw_range; d_yaw < yaw + yaw_range; d_yaw += yaw_grain)
@@ -339,6 +393,10 @@ local void findBest
 			{
 				uint64_t score = evalScore(data, d_yaw, d_pitch, d_roll);
 				std::cout << d_yaw <<','<< d_pitch << ',' << d_roll << ',' << score << std::endl;
+
+				score_t result = {score, {d_yaw, d_pitch, d_roll}};
+				results.push_back(result);
+
 				if( score > best_score)
 				{
 					best_score = score;
@@ -359,16 +417,23 @@ static uint64_t gettime_ns(void)
 }
 
 #if USE_NMS
-local vec3 centroid(const std::vector<vec3> & input)
+local vec3 centroid(const std::vector<score_t> & input, size_t n)
 {
 vec3 r = {0,0,0};
-for( auto i : input)
+for( size_t i = 0; i < n; ++i)
 	{
-	r += i;
+	r += input[i].v;
 	}
-return r / input.size();
+return r / n;
 }
 #endif
+
+local float randf(float min, float max)
+{
+float r =(float)rand() / (float)RAND_MAX;
+float d = max - min;
+return min + d * r;
+}
 
 int main()
 {
@@ -409,12 +474,14 @@ float best_d_yaw = 0, best_d_pitch = 0, best_d_roll = 0;
 float yaw_range = RPM2RADHZ(YAW_SEARCH_RPM);
 float pitch_range = RPM2RADHZ(PITCH_SEARCH_RPM);
 float roll_range = RPM2RADHZ(ROLL_SEARCH_RPM);
-#define YAW_CHUNK 10
-#define PITCH_CHUNK 20
-#define ROLL_CHUNK 8
-float yaw_step = 2 * yaw_range / YAW_CHUNK / 3;
-float pitch_step = 2 * pitch_range / PITCH_CHUNK / 3;
-float roll_step = 2 * roll_range / ROLL_CHUNK / 3;
+#define YAW_CHUNK 12
+#define PITCH_CHUNK 25
+#define ROLL_CHUNK 10
+float yaw_step = yaw_range / YAW_CHUNK;
+float pitch_step = pitch_range / PITCH_CHUNK;
+float roll_step = roll_range / ROLL_CHUNK;
+
+std::vector<score_t> results;
 
 #if USE_HOT_GARBAGE
 //An example solver(hot-garbage approach)
@@ -422,13 +489,14 @@ static const char * const stage_names[]={"coarse","mid","fine","ultra","hyper", 
 
 for( unsigned refine = 0; refine < 5; refine++)
 	{
-	findBest
+	findBestCoarse
 		(
 		data,
 		best_d_yaw, yaw_range, yaw_step,
 		best_d_pitch, pitch_range, pitch_step,
 		best_d_roll, roll_range, roll_step,
-		best_score, best_d_yaw, best_d_pitch, best_d_roll
+		best_score, best_d_yaw, best_d_pitch, best_d_roll,
+		results
 		);
 	yaw_range = yaw_step * 2;
 	pitch_range = pitch_step * 2;
@@ -449,21 +517,210 @@ Pass1: Because NMS gets stuck in local maxima easily, we brute-force scan for th
 Pass2: Nelder-Mead Simplex climbs the gradient hill formed by alinging more and more dimples more closely.
 Pass3: profit???
 */
-static const char * const stage_names[]={"coarse","mid","fine","ultra","hyper", "!!!"};
-
-findBest
+findBestCoarse
 	(
 	data,
 	best_d_yaw, yaw_range, yaw_step,
 	best_d_pitch, pitch_range, pitch_step,
 	best_d_roll, roll_range, roll_step,
-	best_score, best_d_yaw, best_d_pitch, best_d_roll
+	best_score, best_d_yaw, best_d_pitch, best_d_roll,
+	results
 	);
 std::cerr<<"brute"<<"fix:"
 	<<RADHZ2RPM(best_d_yaw)<<','
 	<<RADHZ2RPM(best_d_pitch)<<','
 	<<RADHZ2RPM(best_d_roll)<<':'
 	<<best_score<<std::endl;
+
+//See: https://en.wikipedia.org/wiki/Nelder%E2%80%93Mead_method#One_possible_variation_of_the_NM_algorithm
+//Except: ours is inverted(hill climber rather than minimizer)
+std::sort(results.begin(), results.end(), std::greater<>());
+std::vector<score_t> nms_state;
+//Grab top N
+for(size_t i = 0; i < 15; ++i)
+	{
+	nms_state.push_back(results[i]);
+	}
+
+for(unsigned i = 0; i < nms_state.size(); ++i)
+	{
+	std::cout << nms_state[i].v.x <<',' << nms_state[i].v.y << ',' << nms_state[i].v.z << ',' << nms_state[i].score << std::endl;
+	}
+
+//Manual loop count for now.
+for(unsigned i = 0; i < 30; ++i)
+	{
+	//Step 1: sort the list.
+	std::sort(nms_state.begin(), nms_state.end(), std::greater<>());
+
+	//Debug: print out top entries
+	std::cerr << "NMS" << i << std::endl;
+	for(unsigned i = 0; i < nms_state.size(); ++i)
+		{
+		std::cerr << '\t'
+		<< "score: "<<nms_state[i].score << ' '
+		<< RADHZ2RPM(nms_state[i].v.x) << ' '
+		<< RADHZ2RPM(nms_state[i].v.y) << ' '
+		<< RADHZ2RPM(nms_state[i].v.z) << std::endl;
+		}
+
+	//Step 2: centroid for everything but last
+	vec3 x0 = centroid(nms_state, nms_state.size()-1);
+	score_t xbest = nms_state.front();
+	score_t xworst = nms_state.back();
+
+	std::cerr << '\t'
+		<< "x0: "
+		<< RADHZ2RPM(x0.x) << ' '
+		<< RADHZ2RPM(x0.y) << ' '
+		<< RADHZ2RPM(x0.z) << std::endl;
+
+	//Step 3: Reflection
+	static const float alpha = .25f;
+	score_t xr;
+	xr.v = x0 + alpha * (x0 - xworst.v);
+	xr.score = evalScore(data, xr.v);
+
+	std::cerr << '\t'
+		<< "xr:    "<<xr.score << ' '
+		<< RADHZ2RPM(xr.v.x) << ' '
+		<< RADHZ2RPM(xr.v.y) << ' '
+		<< RADHZ2RPM(xr.v.z) << std::endl;
+
+	std::cerr <<"nms xbest.score=" << xbest.score << " xr.score=" <<xr.score <<" xworst.score=" << xworst.score << std::endl;
+	if(xr.score < xbest.score && xr.score > xworst.score)
+		{
+		//if we are better than worst
+		std::cerr << "reflecting" << std::endl;
+		nms_state.back() = xr;
+		std::cout << xr.v.x <<','<< xr.v.y << ',' << xr.v.z << ',' << xr.score << std::endl;
+		continue;
+		}
+
+	//Step 4: Expansion
+	if(xr.score > xbest.score)
+		{
+		std::cerr << "expanding" << std::endl;
+		static const float gamma = 1.25f;
+		score_t xe;
+		xe.v = x0 + gamma * (xr.v - x0);
+		xe.score = evalScore(data, xe.v);
+		nms_state.back() = xe.score > xr.score ? xe : xr;
+		continue;
+		}
+
+	//Step 5: Contraction
+	const float rho = .25;
+	if(xr.score >= xworst.score)
+		{
+		score_t xc;
+		xc.v = x0 + rho * (xr.v - x0);
+		xc.score = evalScore(data, xc.v);
+		if(xc.score > xr.score)
+			{
+			nms_state.back() = xc;
+			std::cerr << "contracting" << std::endl;
+			continue;
+			}
+		}
+	else
+		{
+		score_t xc;
+		xc.v = x0 + rho * (xworst.v - x0);
+		xc.score = evalScore(data, xc.v);
+		if(xc.score > xworst.score)
+			{
+			nms_state.back() = xc;
+			std::cerr << "contracting" << std::endl;
+			continue;
+			}
+		}
+
+	//Step 6: Shrink
+	std::cerr << "shrinking" << std::endl;
+	for(size_t i = 1; i < nms_state.size(); ++i)
+		{
+		score_t xtemp;
+		xtemp.v = xbest.v + .25f * (nms_state[i].v - xbest.v);
+		xtemp.score = evalScore(data, xtemp.v);
+		nms_state[i] = xtemp;
+		}
+	}
+
+#elif USE_RWALK
+/*A hybrid brute-walker
+Pass3: profit???
+*/
+findBestCoarse
+	(
+	data,
+	best_d_yaw, yaw_range, yaw_step,
+	best_d_pitch, pitch_range, pitch_step,
+	best_d_roll, roll_range, roll_step,
+	best_score, best_d_yaw, best_d_pitch, best_d_roll,
+	results
+	);
+std::cerr<<"brute"<<"fix:"
+	<<RADHZ2RPM(best_d_yaw)<<','
+	<<RADHZ2RPM(best_d_pitch)<<','
+	<<RADHZ2RPM(best_d_roll)<<':'
+	<<best_score<<std::endl;
+
+//See: https://en.wikipedia.org/wiki/Nelder%E2%80%93Mead_method#One_possible_variation_of_the_NM_algorithm
+//Except: ours is inverted(hill climber rather than minimizer)
+std::sort(results.begin(), results.end(), std::greater<>());
+std::vector<score_t> nms_state;
+//Grab top N
+for(size_t i = 0; i < 25; ++i)
+	{
+	nms_state.push_back(results[i]);
+	}
+
+//Manual loop count for now.
+for(unsigned i = 0; i < 10000; ++i)
+	{
+	auto & xworst = nms_state.back();
+	float min_x = INFINITY,min_y = INFINITY,min_z=INFINITY,max_x=-INFINITY,max_y=-INFINITY,max_z=-INFINITY;
+	for(auto i : nms_state)
+		{
+		if(i.v.x < min_x) min_x = i.v.x;
+		if(i.v.x > max_x) max_x = i.v.x;
+		if(i.v.y < min_y) min_y = i.v.y;
+		if(i.v.y > max_y) max_y = i.v.y;
+		if(i.v.z < min_z) min_z = i.v.z;
+		if(i.v.z > max_z) max_z = i.v.z;
+		}
+
+	float dx = max_x - min_x;
+	float dy = max_y - min_y;
+	float dz = max_z - min_z;
+	max_x += .1 * dx;
+	max_y += .1 * dy;
+	max_z += .1 * dz;
+	min_x -= .1 * dx;
+	min_y -= .1 * dy;
+	min_z -= .1 * dz;
+
+	for(unsigned j = 0; j < 10000; ++j)
+		{
+		score_t xr;
+		xr.v.x = randf(min_x,max_x);
+		xr.v.y = randf(min_y,max_y);
+		xr.v.z = randf(min_z,max_z);
+		xr.score = evalScore(data, xr.v);
+		if(xr.score >= xworst.score)
+			{
+			std::cerr<<"derping:\t"<<i<<",\t"<<j<<",\t"<<xr.score<<std::endl;
+			std::cout << xr.v.x <<','<< xr.v.y << ',' << xr.v.z << ',' << xr.score << std::endl;
+
+			//Step 1: sort the list.
+			xworst = xr;
+			std::sort(nms_state.begin(), nms_state.end(), std::greater<>());
+			break;
+			}
+		}
+
+	}
 
 #elif USE_PROFILER
 //Infinite loop for profiling
