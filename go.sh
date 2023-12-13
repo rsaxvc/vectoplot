e7eb9bfe814b48a2497d4c6d4a04c23baa536b31
Richard Allen <rsaxvc@rsaxvc.net>
2023-12-12 20:54:15 -0600

    go fast

--- go.sh
+++ go.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-g++ main.cpp -Wall -Wno-psabi -g -pg -Og && ./a.out  | tee output.csv | less && ./plot.py
+#profiling -pg
+g++ main.cpp -Wall -Wno-psabi -g -O2 && ./a.out 2>logerr.txt | tee output.csv | less && ./plot.py
