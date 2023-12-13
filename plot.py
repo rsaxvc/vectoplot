97bc725436713a29d94545efc8abdcc28ac18bef
Richard Allen <rsaxvc@rsaxvc.net>
2023-12-12 20:54:06 -0600

    3d plots!

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
 yaw=[]
 pitch=[]
@@ -14,14 +15,34 @@ with open('output.csv', newline='') as csvfile:
 		yaw.append(60*float(row[0])/pipi)
 		pitch.append(60*float(row[1])/pipi)
 		roll.append(60*float(row[2])/pipi)
-		score.append(int(row[3]))
+		score.append(float(row[3]))
+yaw = np.array(yaw, np.float32)
+pitch = np.array(pitch, np.float32)
+roll = np.array(roll, np.float32)
+score = np.array(score, np.float32)
 
-# plot
-plt.plot(yaw, score)
-plt.show()
 
-plt.plot(pitch, score)
-plt.show()
+axes={"yaw":yaw,"pitch":pitch,"roll":roll,"score":score}
 
-plt.plot(roll, score)
-plt.show()
+def plot2d(x,y):
+	plt.xlabel(x)
+	plt.ylabel(y)
+	plt.scatter(axes[x], axes[y], marker="+")
+	plt.show()
+
+def plot3d(x, y, z):
+	fig = plt.figure()
+	ax = fig.add_subplot(projection='3d')
+	ax.set_xlabel(x)
+	ax.set_ylabel(y)
+	ax.set_zlabel(z)
+	ax.scatter(axes[x], axes[y], axes[z], marker="+")
+	plt.show()
+
+
+#plot2d('yaw','score')
+#plot2d('pitch','score')
+#plot2d('roll','score')
+plot3d('yaw', 'pitch', 'score')
+plot3d('pitch', 'roll', 'score')
+plot3d('roll', 'yaw', 'score')
