4de83ce6761cda731d9d6f426345c25f36d44694
Richard Allen <rsaxvc@rsaxvc.net>
2023-12-12 21:09:25 -0600

    .

--- go.sh
+++ go.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #profiling -pg
-g++ main.cpp -Wall -Wno-psabi -g -O2 && ./a.out 2>logerr.txt | tee output.csv | less && ./plot.py
+g++ main.cpp -Wall -Wno-psabi -g -O2 && ./a.out 2>logerr.txt > output.csv && ./plot.py
